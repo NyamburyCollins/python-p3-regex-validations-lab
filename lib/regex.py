ac6b90d29ad92bf1ed9a16e972fd4cf4ccec09da
*Collins <nyamburycollins@mail.com>
2025-05-23 07:30:41 +0300

    Completed

--- lib/regex.py
+++ lib/regex.py
@@ -3,10 +3,10 @@ import re
 # NOTE: There are only a few tests included, so multiple solutions will work.
 # Feel free to encourage students to find oversights and add tests to this lab!
 
-name = r""
+name = r"John Cena"
 name_regex = re.compile(name)
 
-phone_number = r""
+phone_number = r"555-555-5555"
 phone_regex = re.compile(phone_number)
 
 email_address = r""
